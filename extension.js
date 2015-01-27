d5e8b46074c7f6131ce8fc7c5f1aa68b62f32026
cooljddj <cooljddj@gmail.com>
2015-01-27 15:09:43 +1100

    Update extension.js

--- extension.js
+++ extension.js
@@ -22,9 +22,9 @@
             type: 'exact',
             functionality: function(chat, cmd){
                 if(this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                if( !bot.commands.executable(this.rank, chat) ) return void (0);
+                if( !bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat('/me This Is A Test Command');
+                    API.sendChat("/me This Is A Test Command");
                 }
             }
         };
