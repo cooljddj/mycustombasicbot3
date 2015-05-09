a40d47302804854aa7943b32b2abb74fc3d98239
cooljddj <cooljddj@gmail.com>
2015-05-09 15:25:13 +1000

    Update extension.js

--- extension.js
+++ extension.js
@@ -36,7 +36,7 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "TrapBot",
+        botName: "The Trap Bot",
         language: "english",
         chatLink: "https://rawgit.com/cooljddj/mybasicBot/master/lang/en.json",
         maximumAfk: 120,
