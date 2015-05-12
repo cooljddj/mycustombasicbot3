238476b878655e18cb3ac5ca58278c1fe78e168e
cooljddj <cooljddj@gmail.com>
2015-05-12 15:45:05 +1000

    Update to Trap Bot

--- extension.js
+++ extension.js
@@ -27,6 +27,19 @@
                 }
             }
         };
+        
+        bot.commands.baconCommand = {
+            command: 'nuts',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me Deez Nuts!");
+                }
+            }
+        };
 
         //Load the chat package again to account for any changes
         bot.loadChat();
@@ -79,7 +92,7 @@
         website: "http://alltrapnation.com/",
         intervalMessages: ["Like us on Facebook: http://facebook.com/alltrapnation", "Sub to us on Youtube: https://www.youtube.com/user/AllTrapNation", "Follow us on twitter: http://twitter.com/alltrapnation"],
         messageInterval: 5,
-        songstats: true,
+        songstats: false,
         commandLiteral: "!",
         blacklists: {
             NSFW: "https://rawgit.com/cooljddj/mycustombasicbot/master/blacklists/ExampleNSFWlist.json",
