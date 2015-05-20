e8cdffb2c540c348d082538f585b81841d9eda16
cooljddj <cooljddj@gmail.com>
2015-05-21 08:37:51 +1000

    Update extension.js

--- extension.js
+++ extension.js
@@ -28,19 +28,6 @@
             }
         };
         
-        bot.commands.deeCommand = {
-            command: 'nuts',  //The command to be called. With the standard command literal this would be: !bacon
-            rank: 'user', //Minimum user permission to use the command
-            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
-            functionality: function (chat, cmd) {
-                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                if (!bot.commands.executable(this.rank, chat)) return void (0);
-                else {
-                    API.sendChat("/me Deez Nuts!");
-                }
-            }
-        };
-        
         bot.commands.fbgCommand = {
             command: 'fbg',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
@@ -92,7 +79,7 @@
         autodisable: true,
         commandCooldown: 30,
         usercommandsEnabled: true,
-        lockskipPosition: 3,
+        lockskipPosition: 2,
         lockskipReasons: [
             ["theme", "This song does not fit the room theme. "],
             ["op", "This song is on the OP list. "],
