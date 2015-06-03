0f0dbbb8589b161f753999229c1a30c561e87911
cooljddj <cooljddj@gmail.com>
2015-06-03 17:55:49 +1000

    Update extension.js

--- extension.js
+++ extension.js
@@ -23,7 +23,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("Java Bot Made By Yemasthui, Edited By COOLJ|The Bouncer|xBytez");
+                    API.sendChat("Java Bot Made By Yemasthui, Edited By COOLJ|Trap Nation | cooljddj|The Nation | xBytez");
                 }
             }
         };
@@ -50,6 +50,19 @@
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
                     API.sendChat("/me We use the RCS plugin in this room. Get it here: https://rcs.radiant.dj/");
+                }
+            }
+        };
+        
+        bot.commands.changelog = {
+            command: 'changelog',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'mod', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("Change Log: -Removed AFK Removal +Added DC limit to 120 *Fixed !credits command");
                 }
             }
         };
